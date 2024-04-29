c92425bd62584f83518e076ee32d3ce4121c18e1
ALBERT <ishengomaalbert@gmail.com>
2024-04-29 11:19:07 +0300

    Custom app properties & Spring Boot properties

--- src/main/java/com/example/demo/rest/FunnyRestController.java
+++ src/main/java/com/example/demo/rest/FunnyRestController.java
@@ -1,10 +1,19 @@
 package com.example.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunnyRestController {
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @Value("${team.coach}")
+    private String coachName;
+
+
     @GetMapping("/")
     public String sayHello(){
         return "Hello World!";
@@ -14,4 +23,7 @@ public class FunnyRestController {
     public String getWorkout(){
         return "Do 50 pushups";
     }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){ return "Team Name: "+teamName+", CoachName: "+coachName; }
 }
