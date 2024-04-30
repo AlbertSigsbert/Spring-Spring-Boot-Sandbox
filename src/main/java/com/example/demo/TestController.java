96ebf5f7a53e5b4ac686f92b601b4cce742cb9e5
ALBERT <ishengomaalbert@gmail.com>
2024-04-30 08:54:28 +0300

    Constructor Injection

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    //private field for the dependency
+    private Coach myCoach;
+    @Autowired
+    public TestController(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
